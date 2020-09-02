fe7497fe0fdd9fedaa30ba6b01307e5d1ff84966
Carlos Steeven Jaramillo <carlosjaramillo@percepcion.digital>
2020-09-01 20:13:20 -0500

    Version 0.1

---
+++ app/src/main/java/com/example/ejemplo1listview/ForeachActivity.java
@@ -0,0 +1,27 @@
+package com.example.ejemplo1listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ForeachActivity extends AppCompatActivity {
+
+    protected TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_foreach);
+
+        textView = findViewById(R.id.textView);
+        
+        String[] arraycito = {"Julian", "Miguel", "Pablo"};
+        String arraycitoS = "";
+
+        for (int i = 0; i < arraycito.length; i++) {
+            arraycitoS += arraycito[i] + "\n ----------------------- \n";
+        }
+
+        textView.setText(arraycitoS);
+    }
+}
